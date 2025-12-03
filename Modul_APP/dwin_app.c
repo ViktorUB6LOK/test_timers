028696d1e90a0884a821e7f07526dff9a0a08117
ViktorUB6LOK_W <ub6lok@yandex.ru>
2025-12-03 12:22:50 +0300

    Add func_1 для режима1 ...

--- Modul_APP/dwin_app.c
+++ Modul_APP/dwin_app.c
@@ -29,7 +29,7 @@ void DWIN_Start_page() {
 	HAL_Delay(400);
 }
 
-void DWIN_Select_mode() { // перенос в основной цикл
+void DWIN_Select_mode() {
 	bool flag_select_mode = false;
 	uint16_t adress_parsing = 0;
 	uint16_t data_parsing   = 0;
@@ -49,17 +49,51 @@ void DWIN_Select_mode() { // перенос в основной цикл
 
 	if (adress_parsing == 0x5001) {
 		switch (data_parsing) {
-		      case (0x0001):
-		          goToPageDWIN(2);
+		      case (0x0001):              // режим 1 (генератор flowmeter)
+		          goToPageDWIN(2);        // переключение на страницу режима 1
+		          func_1 ();
 		    	  break;
-		      case (0x0002):
-		          goToPageDWIN(3);
+		      case (0x0002):              // режим 2 (генератор flowmeter)
+		          goToPageDWIN(3);        // переключение на страницу режима 2
 		    	  break;
-		      case (0x0003):
-		          goToPageDWIN(4);
+		      case (0x0003):              // режим 3 (генератор flowmeter)
+		          goToPageDWIN(4);        // переключение на страницу режима 3
 		      	  break;
 		      default:
 		    	  break;
 	}
   }
+}
+
+void func_1 (){
+	    bool flag_func_1 = false;
+		uint16_t adress_parsing = 0;
+		uint16_t data_parsing   = 0;
+		uint16_t setting_ratio_flowmeter = 0;    // коэфф. литр в мин - возможно они не нужны
+		uint16_t data_flowmeter = 0;             // поток  - возможно они не нужны
+
+		while (!flag_func_1){                         // пока не будет нажата кнопка выбора режима крутимся в цикле
+			if (flag_dwin_tx_IT) {
+							flag_dwin_tx_IT = false;
+							parsingDWIN();
+							adress_parsing = readDataDWIN.parsingDataDWIN.data[0] << 8
+													| readDataDWIN.parsingDataDWIN.data[1];
+							data_parsing = readDataDWIN.parsingDataDWIN.data[3] << 8
+													| readDataDWIN.parsingDataDWIN.data[4];
+
+							switch (adress_parsing) {
+									      case (dwin_adress_flowmeter_setting):      // если нажали на выбор коэфф. литр/мин
+									    		  // передаем в функцию обработки поток + коэфф.
+									    	  break;
+									      case (dwin_adress_flowmeter):            // если нажата - изменяем поток и даем команду на AD9833
+	                                            	// передаем в функцию обработки поток + коэфф.
+									    	  break;
+									      case (dwin_adress_button_back):          // если нажата выбор режима - true и выходим из функции
+	                                           	flag_func_1 = true;
+									      	  break;
+									      default:
+									    	  break;
+								}
+				}
+		}
 }
--- Modul_APP/dwin_app.h
+++ Modul_APP/dwin_app.h
@@ -16,6 +16,9 @@
 #define dwin_adress_flowmeter_setting  0x1010
 #define dwin_adress_speedmeter_setting 0x2010
 
+#define dwin_adress_button_back 0x0000
+// + указать адресс ( значение кнопки перехода в выбор режима)
+
 struct DWIN_VAR {             // объявление структура в main.c
 	   uint16_t adress;
 	   uint16_t data;
@@ -24,6 +27,7 @@ struct DWIN_VAR {             // объявление структура в main
 void DWIN_Set_var(void);
 void DWIN_Start_page(void);
 void DWIN_Select_mode (void);
+void func_1 (void);
 
 
 #endif /* DWIN_APP_H_ */
