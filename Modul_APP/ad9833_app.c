19149b9de52c5348a488870c3f60d2a93cd8a7b0
ViktorUB6LOK <ub6lok@yandex.ru>
2025-12-07 11:04:33 +0300

    внесены изменения в send_data_to_ad9833-убраны проверки - не нужны они!

--- Modul_APP/ad9833_app.c
+++ Modul_APP/ad9833_app.c
@@ -20,39 +20,47 @@ void INIT_AD9833 () {
 
 
 // ---------   функция передачи значения параметров в модуль AD9833 --------------------------
+// Функция использует данные с dwin, проверка обновления данных не нужна -
+// dwin передает только измененные данные по сравнению с предыдущей передачей.
+// проверка нужна будет если данные поступают из вне или от генератора ...
+// проверка изменений необходима чтоб лишний раз не писать данные в AD9833!!!
+// dwin передает каждый раз новые данные в диапазоне 0-200 л/мин (сам ограничивает их)
+
 void send_data_to_ad9833 (uint16_t data_flowmeter, uint16_t setting_ratio_flowmeter){
-	static uint16_t old_data_flowmeter = 0;
-	static uint16_t old_setting_ratio_flowmeter = 0;
-	uint16_t flowmeter_freq_max = (setting_ratio_flowmeter * data_flowmeter_max) / 60; // max частота
+//	static uint16_t old_data_flowmeter = 0;
+//	static uint16_t old_setting_ratio_flowmeter = 0;
+//	uint16_t flowmeter_freq_max = (setting_ratio_flowmeter * data_flowmeter_max) / 60; // max частота
 	uint16_t flowmeter_freq = (setting_ratio_flowmeter * data_flowmeter) / 60;         // расчет частоты генератора
 
-	if (((data_flowmeter != old_data_flowmeter) || (setting_ratio_flowmeter != old_setting_ratio_flowmeter)) && (flowmeter_freq <= flowmeter_freq_max)) {
+//	if (((data_flowmeter != old_data_flowmeter) || (setting_ratio_flowmeter != old_setting_ratio_flowmeter)) && (flowmeter_freq <= flowmeter_freq_max))
+  {
 	AD9833_SetWaveData(flowmeter_freq, 0); // передача частоты на модуль, 1- меандр
-	   old_data_flowmeter = data_flowmeter;
-	   old_setting_ratio_flowmeter = setting_ratio_flowmeter;
+//	   old_data_flowmeter = data_flowmeter;
+//	   old_setting_ratio_flowmeter = setting_ratio_flowmeter;
 	}
 }
 
 // ------------ функция передачи значения параметров в модуль AD9833_2 -----------------------
 void send_data_to_ad9833_2 (uint16_t data_speedmeter, uint16_t setting_ratio_speedmeter){
-	    static uint16_t old_data_speedmeter = 0;
-		static uint16_t old_setting_ratio_speedmeter = 0;
-		float speedmeter_freq_max = data_speedmeter_max / (3.6 * setting_ratio_speedmeter / 100 ); // max частота
+//	    static uint16_t old_data_speedmeter = 0;
+//		static uint16_t old_setting_ratio_speedmeter = 0;
+//		float speedmeter_freq_max = data_speedmeter_max / (3.6 * setting_ratio_speedmeter / 100 ); // max частота
 		float speedmeter_freq = ((float)(data_speedmeter/10)) / ((float)(3.6 * setting_ratio_speedmeter / 100 ));         // расчет частоты генератора
 
-		if (((data_speedmeter != old_data_speedmeter) || (setting_ratio_speedmeter != old_setting_ratio_speedmeter)) && (speedmeter_freq <= speedmeter_freq_max)) {
+//		if (((data_speedmeter != old_data_speedmeter) || (setting_ratio_speedmeter != old_setting_ratio_speedmeter)) && (speedmeter_freq <= speedmeter_freq_max))
+	{
 		AD9833_SetWaveData_2(speedmeter_freq, 0); // передача частоты на модуль, 2- меандр
-		   old_data_speedmeter = data_speedmeter;
-		   old_setting_ratio_speedmeter = setting_ratio_speedmeter;
+//		   old_data_speedmeter = data_speedmeter;
+//		   old_setting_ratio_speedmeter = setting_ratio_speedmeter;
 		}
 }
 
 void power_off_ad9833 (){
 	// функция выключения модуля AD9833;
-	AD9833_SetWaveData(0.0f, 0);  // пока так - далее переделать что откл модуль совсем!
+	AD9833_SetWaveData(.0f, 0);  // пока так - далее переделать что откл модуль совсем!
 }
 
 void power_off_ad9833_2 (){
 	// функция выключения модуля AD9833;
-	AD9833_SetWaveData_2(0.0f, 0);  // пока так - далее переделать что откл модуль совсем!
+	AD9833_SetWaveData_2(.0f, 0);  // пока так - далее переделать что откл модуль совсем!
 }
